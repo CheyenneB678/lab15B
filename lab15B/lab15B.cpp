ccccaa66641856d5c8a298c6d7bdc8e92aab9753
buchert70427 <buchert70427@B124-19>
2025-04-15 09:36:26 -0400

    Template function for floating-point types (float, double)

--- lab15B/lab15B.cpp
+++ lab15B/lab15B.cpp
@@ -4,3 +4,15 @@
 *Lab 15 B
 *4/15/2025
 *******************/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Template function
+template <typename T>
+T half(T value) {
+	return value / 2;
+}
+
